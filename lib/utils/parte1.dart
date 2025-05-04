eac381820b27b192baedc366d3ab3f0dc1111086
Edilson Ribeiro <edilsondev@outlook.com>
2025-05-04 15:00:31 -0400

    Add analyzeList function and its unit tests

--- lib/utils/parte1.dart
+++ lib/utils/parte1.dart
@@ -1,7 +1,28 @@
-void main() {
-  List<int> numbers = [1, 2, 3, 4, 5];  // Declaração correta da lista
-  
-  for (var number in numbers) {  // Laço de repetição
-    print(number);
+Map <String, int> analyzeList(List<int> numbers)
+ {
+  int smallest = numbers[0];
+  int largest = numbers[0];
+  int sum = 0;
+
+  for (var number in numbers) {
+    if (number < smallest) smallest = number;
+    if (number > largest) largest = number;
+    sum += number;
   }
+
+  return {
+    'smallest': smallest,
+    'largest': largest,
+    'sum': sum,
+  };
+ }
+
+void main() {
+List<int> numbers = [1, 2, 3, 4, 5];
+
+  final result = analyzeList(numbers);
+
+  print('The smallest number is: ${result['smallest']}');
+  print('The largest number is: ${result['largest']}');
+  print('The sum is: ${result['sum']}');
 }
